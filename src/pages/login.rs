5246a6eed55201334efaf9ac69d58cf56ff0827e
spinsir45 <github.o02wq@passmail.net>
2024-10-04 13:13:31 -0600

    backend gets username and password

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,3 +1,5 @@
+use core::hash;
+use std::hash::{ DefaultHasher, Hash, Hasher };
 use actix_web::{ get, web, Error, HttpResponse, Responder, http };
 use actix_identity::IdentityMiddleware;
 use serde::{Deserialize, Serialize};
@@ -10,7 +12,17 @@ pub struct LoginForm {
 
 /// Authenticates the user and creates a session.
 pub async fn auth_user(form_data: web::Form<LoginForm>) -> HttpResponse {
+    // Put form data into struct
     let form = form_data.into_inner();
-    println!("username: {:?} || password: {}", form.username, form.password);
+
+    // Hash the password
+    let mut hasher = DefaultHasher::new();
+    form.password.hash(&mut hasher);
+    let _value = hasher.finish();
+
+    // Check if the user is an authentic user
+
+    
+    // Return response
     HttpResponse::Ok().finish()
 }
