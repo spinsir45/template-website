701ac416b7978637630c48f640039baf73cae69b
spinsir45 <github.o02wq@passmail.net>
2024-10-06 20:06:26 -0600

    login authenticates users

--- src/pages/login.rs
+++ src/pages/login.rs
@@ -1,8 +1,8 @@
-use core::hash;
 use std::hash::{ DefaultHasher, Hash, Hasher };
 use actix_web::{ get, web, Error, HttpResponse, Responder, http };
 use actix_identity::IdentityMiddleware;
 use serde::{Deserialize, Serialize};
+use toml::value::{Table, Value};
 
 #[derive(Serialize, Deserialize)]
 pub struct LoginForm {
@@ -12,6 +12,7 @@ pub struct LoginForm {
 
 /// Authenticates the user and creates a session.
 pub async fn auth_user(form_data: web::Form<LoginForm>) -> HttpResponse {
+    let mut authorized: bool = false;
     // Put form data into struct
     let form = form_data.into_inner();
 
@@ -21,8 +22,20 @@ pub async fn auth_user(form_data: web::Form<LoginForm>) -> HttpResponse {
     let hashed_password = hasher.finish();
 
     // Check if the user is an authentic user
-    
+    let toml_string = std::fs::read_to_string("/home/spinsir/Projects/template-website/Users.toml").unwrap();
+    let toml_value: Value = toml::from_str(&toml_string).unwrap();
+    let table = toml_value.as_table().unwrap();
+    let users = table.get("users").unwrap();
+    if let Some(value) = users.get(form.username) {
+        if value.as_str().unwrap() == hashed_password.to_string() {
+            authorized = true;
+        }
+    }
     
     // Return response
-    HttpResponse::Ok().finish()
+    if authorized {
+        HttpResponse::Ok().finish()
+    } else {
+        HttpResponse::Unauthorized().finish()
+    }
 }
